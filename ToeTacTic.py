ac872b508cbc40683cecda0e68913b097a6b6f77
claySorrick <billiard117@yahoo.com>
2017-08-03 13:03:01 -0700

    init dev branch, Readme, and outline

---
+++ ToeTacTic.py
@@ -0,0 +1,33 @@
+"""
+Tic Tac Toe game - 2 players (no computer opponent)
+ -wait for a player's input
+ -validates the input, makes the move
+ -output updated board
+ -check win condition
+"""
+#Put imports here (if any are needed)
+
+
+#CONSTANTS and Global variables here
+
+
+#Define check_win() function to check if a player has won - returns T or F
+
+
+#Define is_input_valid() function to check player's input - returns T or F
+
+
+#Define player_input() function to receive a player's move
+
+
+#Define print_board() function to print the board to the terminal
+
+
+#Define game_over() function to output the winner and end the game
+
+
+#Define main() function that will start the game and handle the main loop
+
+
+#call main()
+main()
