133a9ee527c552706679bcff2f0fc21c095d6a09
claySorrick <billiard117@yahoo.com>
2017-08-15 17:27:36 -0700

    2 players, can make moves
    updated board to be 1-9
    loops through 2 player's inputs
    validates inputs
    makes changes to board

--- ToeTacTic.py
+++ ToeTacTic.py
@@ -9,32 +9,60 @@ Tic Tac Toe game - 2 players (no computer opponent)
 
 
 #CONSTANTS and Global variables here
-
-#width and height of board
 w = 3
 h = 3
-
-#creating an empty board array
-board = [[' ' for x in range(w)] for y in range(h)]
+p1 = "X"
+p2 = "O"
+#Declare board array of 1-9; still a 0 based array
+board = [str(x+1) for x in range(w*h)]
 
 #Define check_win() function to check if a player has won - returns T or F
 def check_win():
 	return False
 
 #Define is_input_valid() function to check player's input - returns T or F
-def is_input_valid(x,y):
-	return True
+def is_input_valid(x):
+	try:
+		x = int(x)
+	except:
+		print("NOT A NUMBER")
+		return False
+	if 0<x<=w*h:
+		if board[x-1] != p1 and board[x-1] != p2:
+			return True
+		print("UNAVAILABLE POSITION")
+	else:
+		print("OUTSIDE BOUNDS")
+	return False
 
 #Define player_input() function to receive a player's move
-def player_input():
+def player_input(turn):
+	valid = False
+	while(not valid):
+		print("Player %s's turn. Enter position:" % turn)
+		i_x = input("Position: ")
+		if i_x == 'q':
+			exit()
+		valid = is_input_valid(i_x)
+	#make move on the board
+	board[int(i_x)-1] = turn
+	#change turn
+	if turn == p1:
+		return p2
+	else:
+		return p1
 
 
 #Define print_board() function to print the board to the terminal
 def print_board():
-	for row in board:
-		for c in row[0:len(row)-1]:
-			print(c + " | ", end="")
-		print(row[len(row)-1])
+	for index,p in enumerate(board):
+		if (index+1) % 3 == 0 and index>0:
+			print(p)
+			if index<8:
+				print("---------")
+		else:
+			print(p + " | ", end="")
+	print()
 	
 
 #Define game_over() function to output the winner and end the game
@@ -43,9 +71,11 @@ def game_over():
 
 #Define main() function that will start the game and handle the main loop
 def main():
+	turn = p1
 	print("Welcome to a game of Tic Tac Toe, 'q' to quit")
 	print_board()
-
-
+	while(not check_win()):
+		turn = player_input(turn)
+		print_board()
 #call main()
 main()
